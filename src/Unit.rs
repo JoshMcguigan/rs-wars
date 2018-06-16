6f9ab720d8b12b5bbfea9e2cd095a18483976a3b
Josh Mcguigan <joshmcg88@gmail.com>
2018-06-16 10:23:10 -0700

    Implemented tank unit type, and attackers take damage

--- src/Unit.rs
+++ src/Unit.rs
@@ -1,12 +1,24 @@
 pub struct Unit {
     health: u16,
+    attack: f32,
 }
 
 impl Unit {
     pub fn soldier() -> Unit {
         Unit {
             health: 10,
+            attack: 2f32,
         }
+    }
+    pub fn tank() -> Unit {
+        Unit {
+            health: 10,
+            attack: 4f32,
+        }
+    }
+
+    fn get_attack(&self) -> f32 {
+        self.attack * self.health as f32 / 10f32
     }
 
     fn is_alive(&self) -> bool {
@@ -23,7 +35,8 @@ impl Unit {
     }
 
     pub fn attack(&mut self, other: &mut Unit) {
-        other.take_damage(2);
+        other.take_damage(self.get_attack() as u16);
+        self.take_damage((other.get_attack() / 2f32) as u16);
     }
 }
 
@@ -49,13 +62,68 @@ mod tests {
     }
 
     #[test]
-    fn attack(){
+    fn attack_defender_is_more_heavily_damaged(){
         let mut unit1 = Unit::soldier();
         let mut unit2 = Unit::soldier();
 
         unit1.attack(&mut unit2);
 
         assert!(unit1.get_health() > unit2.get_health());
+    }
+
+    #[test]
+    fn attack_tank_is_more_powerful_than_soldier(){
+        let mut attacking_solider = Unit::soldier();
+        let mut attacking_tank = Unit::tank();
+        let mut unit1 = Unit::soldier();
+        let mut unit2 = Unit::soldier();
+
+        attacking_solider.attack(&mut unit1);
+        attacking_tank.attack(&mut unit2);
+
+        assert!(unit1.get_health() > unit2.get_health());
+    }
+
+    #[test]
+    fn attack_attacking_unit_is_also_damaged(){
+        let mut unit1 = Unit::soldier();
+        let mut unit2 = Unit::tank();
+
+        unit1.attack(&mut unit2);
+
+        assert!(unit1.get_health() < 10);
+    }
+
+    #[test]
+    fn attack_damaged_units_are_less_powerful_when_attacking(){
+        let mut unit1 = Unit::soldier();
+        let mut unit2 = Unit::soldier();
+        unit1.attack(&mut unit2);
+        let full_health_damage = 10 - unit2.get_health();
+
+        unit1 = Unit::soldier();
+        unit2 = Unit::soldier();
+        unit1.take_damage(5);
+        unit1.attack(&mut unit2);
+        let half_health_damage = 10 - unit2.get_health();
+
+        assert!(full_health_damage > half_health_damage);
+    }
+
+    #[test]
+    fn attack_damaged_units_are_less_powerful_when_defending(){
+        let mut unit1 = Unit::soldier();
+        let mut unit2 = Unit::tank();
+        unit1.attack(&mut unit2);
+        let full_health_damage = 10 - unit1.get_health();
+
+        unit1 = Unit::soldier();
+        unit2 = Unit::tank();
+        unit2.take_damage(5);
+        unit1.attack(&mut unit2);
+        let half_health_damage = 10 - unit1.get_health();
+
+        assert!(full_health_damage > half_health_damage);
     }
 
 }
